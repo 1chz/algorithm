d2e50efec2647c0aae94bff6872ea64b49316475
shirohoo <shirohoo4102@gmail.com>
2021-09-05 04:15:39 +0900

    Add ArrayList.java

--- src/main/java/collection/ArrayList.java
+++ src/main/java/collection/ArrayList.java
@@ -1,21 +1,80 @@
 package collection;
 
+import java.util.Arrays;
 import java.util.Objects;
 
-public class ArrayList {
-    private final int[] ints;
+public class ArrayList<E> {
 
-    private ArrayList(final int[] ints) {
-        this.ints = ints;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private E[] arrayList;
+
+    private int size;
+
+    public ArrayList() {
+        this.size = 0;
+        this.arrayList = (E[]) new Object[DEFAULT_CAPACITY];
     }
 
-    public static ArrayList create() {
-        return new ArrayList(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+    public ArrayList(final int capacity) {
+        this.size = 0;
+        this.arrayList = (E[]) new Object[capacity];
     }
 
-    public void printHash() {
-        for (int asInt : ints) {
-            System.out.println(Objects.hash(asInt));
+    public E get(final int index) {
+        if (index > size - 1) {
+            throw new IndexOutOfBoundsException("Index is too big !");
         }
+        return arrayList[index];
     }
+
+    public int size() {
+        return this.size;
+    }
+
+    public void add(final E e) {
+        if (size == arrayList.length) {
+            this.arrayList = Arrays.copyOf(arrayList, arrayList.length + DEFAULT_CAPACITY);
+        }
+        this.arrayList[size++] = e;
+    }
+
+    public void add(final int index, final E e) {
+        if (index > size - 1) {
+            throw new IndexOutOfBoundsException("Index is too big !");
+        }
+        if (size == arrayList.length) {
+            this.arrayList = Arrays.copyOf(arrayList, arrayList.length + DEFAULT_CAPACITY);
+        }
+        for (int i = size - 1; i >= index; i--) {
+            this.arrayList[i + 1] = arrayList[i];
+        }
+        this.arrayList[index] = e;
+        size++;
+    }
+
+    public void remove(final int index) {
+        if (index > size - 1) {
+            throw new IndexOutOfBoundsException("Index is too big !");
+        }
+        for (int i = index + 1; i < size; i++) {
+            this.arrayList[i - 1] = arrayList[i];
+        }
+        Objects.isNull(this.arrayList[size--]);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("[ ");
+        for (int i = 0; i < this.size; i++) {
+            if (i == this.size - 1) {
+                sb.append(this.arrayList[i].toString() + " ");
+                break;
+            }
+            sb.append(this.arrayList[i].toString() + ", ");
+        }
+        return sb.append("]")
+                .toString();
+    }
+
 }
--- src/test/java/collection/ArrayListTest.java
+++ src/test/java/collection/ArrayListTest.java
@@ -1,18 +1,59 @@
 package collection;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ArrayListTest {
-    private ArrayList arrayList;
+
+    private ArrayList<String> arrayList;
 
     @BeforeEach
     void setUp() {
-        arrayList = ArrayList.create();
+        arrayList = new ArrayList<>();
+        arrayList.add("shirohoo");
     }
 
     @Test
-    void print() throws Exception {
-        arrayList.printHash();
+    void get() {
+        Assertions.assertThat(arrayList.get(0)).isEqualTo("shirohoo");
+    }
+
+    @Test
+    void size() {
+        Assertions.assertThat(arrayList.size()).isEqualTo(1);
+    }
+
+    @Test
+    void add_1() {
+        arrayList.add("shirohoo2");
+        Assertions.assertThat(arrayList.size()).isEqualTo(2);
+    }
+
+    @Test
+    void add_2() {
+        arrayList.add(0, "shirohoo2");
+        arrayList.add(0, "shirohoo3");
+        assertAll(
+                () -> Assertions.assertThat(arrayList.size()).isEqualTo(3),
+                () -> Assertions.assertThat(arrayList.get(0)).isEqualTo("shirohoo3"),
+                () -> Assertions.assertThat(arrayList.get(1)).isEqualTo("shirohoo2"),
+                () -> Assertions.assertThat(arrayList.get(2)).isEqualTo("shirohoo")
+        );
+
+    }
+
+    @Test
+    void remove() {
+        arrayList.remove(0);
+        Assertions.assertThat(arrayList.size()).isEqualTo(0);
+    }
+
+    @Test
+    void testToString() {
+        arrayList.add("shirohoo2");
+        Assertions.assertThat(arrayList.toString()).isEqualTo("[ shirohoo, shirohoo2 ]");
     }
 }
