d2ece5d964328ad455ccd1822c6d8b3628b6ad70
shirohoo <shirohoo4102@gmail.com>
2021-09-29 11:10:18 +0900

    문자열에서 알파벳 갯수 찾기 패키지 변경

--- src/main/java/practice/string/FindChars.java
+++ src/main/java/practice/string/FindChars.java
@@ -1,4 +1,4 @@
-package practice;
+package practice.string;
 
 public class FindChars {
 
--- src/test/java/practice/string/FindCharsTest.java
+++ src/test/java/practice/string/FindCharsTest.java
@@ -1,10 +1,11 @@
-package practice;
+package practice.string;
 
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import practice.string.FindChars;
 
 /**
  * <pre>
