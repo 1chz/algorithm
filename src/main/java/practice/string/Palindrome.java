b5e63b11e15bc70213ea29aebb537d3deb30a3a5
shirohoo <shirohoo4102@gmail.com>
2021-09-30 09:59:32 +0900

    문자열 - 회문 문자열 (=팰린드롬)
    리팩토링

--- src/main/java/practice/string/Palindrome.java
+++ src/main/java/practice/string/Palindrome.java
@@ -3,8 +3,11 @@ package practice.string;
 public class Palindrome {
 
     public boolean solution(final String sentence) {
-        final String reverseSentence = new StringBuilder(sentence).reverse().toString();
-        return sentence.equalsIgnoreCase(reverseSentence);
+        return sentence.equalsIgnoreCase(
+            new StringBuilder(sentence)
+                .reverse()
+                .toString()
+        );
     }
 
     public boolean solution2(String sentence) {
