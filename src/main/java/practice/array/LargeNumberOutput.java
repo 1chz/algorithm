876a8bb6dca6ba65844924b23c6e611db04cd960
shirohoo <shirohoo4102@gmail.com>
2021-10-15 16:32:59 +0900

    refactor: 배열 - 큰 수 출력하기

--- src/main/java/practice/array/LargeNumberOutput.java
+++ src/main/java/practice/array/LargeNumberOutput.java
@@ -5,28 +5,19 @@ import static java.util.Arrays.stream;
 public class LargeNumberOutput {
 
     public String solution(final int numCount, final String testCase) {
-        String answer = "";
-
         int[] nums = stream(testCase.split(" "))
             .mapToInt(Integer::parseInt)
             .toArray();
 
-        int temp = 0;
+        String answer = String.valueOf(nums[0]);
 
-        for (int i = 0; i < nums.length; i++) {
-            if (i == 0) {
-                answer = append(answer, nums[i]);
-            } else if (nums[i] > temp) {
-                answer = append(answer, nums[i]);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                answer = answer + " " + nums[i];
             }
-            temp = nums[i];
         }
 
-        return answer.trim();
-    }
-
-    private String append(String answer, final int num) {
-        return answer + num + " ";
+        return answer;
     }
 
 }
