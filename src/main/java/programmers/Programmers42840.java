04b3daf084b2d23dcce5eb9dcdc4938256c72eae
shirohoo <shirohoo4102@gmail.com>
2022-04-01 22:58:54 +0900

    :recycle: 프로그래머스 42840 - 모의고사 리팩토링

--- src/main/java/programmers/Programmers42840.java
+++ src/main/java/programmers/Programmers42840.java
@@ -1,31 +1,25 @@
 package programmers;
 
-import static java.util.Objects.isNull;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.IntUnaryOperator;
 import java.util.stream.IntStream;
 
-public class Programmers42840 {
+class Programmers42840 {
+    public static final int[][] PATTERN = new int[][]{
+        {1, 2, 3, 4, 5},
+        {2, 1, 2, 3, 2, 4, 2, 5},
+        {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+    };
 
     public int[] solution(int[] answers) {
-        int[][] people = {
-            {1, 2, 3, 4, 5},
-            {2, 1, 2, 3, 2, 4, 2, 5},
-            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
-        };
-
         Map<Integer, List<Integer>> map = new HashMap<>();
         for (int i = 1; i <= 3; i++) {
-            int score = score(people[i - 1], answers);
-            List<Integer> arr = map.get(score);
-            if (isNull(arr)) {
-                arr = new ArrayList<>();
-            }
+            int score = score(PATTERN[i - 1], answers);
+            List<Integer> arr = map.getOrDefault(score, new ArrayList<>());
             arr.add(i);
             map.put(score, arr);
         }
@@ -33,26 +27,15 @@ public class Programmers42840 {
         List<Integer> scores = new ArrayList<>(map.keySet());
         scores.sort(Collections.reverseOrder());
 
-        List<Integer> top = map.get(scores.get(0));
-        int[] answer = new int[top.size()];
-        Arrays.setAll(answer, top::get);
-        
+        List<Integer> topPeople = map.get(scores.get(0));
+        int[] answer = new int[topPeople.size()];
+        Arrays.setAll(answer, topPeople::get);
         return answer;
     }
 
     private int score(int[] people, int[] answers) {
         return IntStream.range(0, answers.length)
-            .map(isMatch(people, answers))
+            .map(i -> people[i % people.length] == answers[i] ? 1 : 0)
             .sum();
     }
-
-    private IntUnaryOperator isMatch(final int[] people, final int[] answers) {
-        return i -> {
-            if (people[i % people.length] == answers[i]) {
-                return 1;
-            }
-            return 0;
-        };
-    }
-
 }
